f74995786f33cba2d9b53d659ba3c80f4cfdc389
creatixatemp <62591565+creatixatemp@users.noreply.github.com>
2020-03-25 19:03:18 +0800

    updated validation

--- models/form.js
+++ models/form.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 const Schema= mongoose.Schema;
 const formSchema = new Schema({
-        name         : {type: String, maxlength: 100, required: true},
-        email        : {type: String, maxlength: 150, required: true},
-        contact      : {type: String, maxlength: 150, required: true},
-        message      : {type: String, maxlength: 150, required: true},
+        name         : {type: String, minlength:2, maxlength: 100, required: true},
+        email        : {type: String, minlength:2, maxlength: 100, required: true},
+        contact      : {type: Number, minlength:2, maxlength: 15, required: true},
+        message      : {type: String, minlength:2, maxlength: 450, required: true},
                 
 }, { timestamps : { 
     createdAt   : 'created_at',
